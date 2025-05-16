10b17f77241e84756e0959bed8106c8478d021fc
pbay1 <patbay23@outlook.com>
2025-05-16 23:37:29 +0800

    Delete cs directory

--- cs/config/automation.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'rpc_url' => env('NANO_RPC_URL', 'http://[::1]:7076'),
-    'admin_wallet' => env('NANO_ADMIN_WALLET'),
-    'encryption_key' => env('NANO_ENCRYPTION_KEY'),
-    'withdrawal_fee' => env('NANO_WITHDRAWAL_FEE', 0.01),
-];
